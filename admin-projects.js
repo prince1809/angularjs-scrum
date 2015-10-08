cda6c303a33d56dabb103b0add448995a92d7284
Prince Kumar <prince18pat@gmail.com>
2015-10-09 00:45:46 +0900

    Initial Directorty

---
+++ admin-projects.js
@@ -0,0 +1,10 @@
+angular.module('admin-projects',[])
+
+.controller('ProjectsListCtrl', function($scope){
+   // controller code go here 
+})
+
+
+.controller('ProjectEditCtrl', function($scope){
+   // controller's code go here 
+});
