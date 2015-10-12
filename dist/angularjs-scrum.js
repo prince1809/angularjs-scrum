ffb764806339ab8760e7042ae316dee729c86c6d
Prince Kumar <prince18pat@gmail.com>
2015-10-12 16:40:58 +0900

    Client side configuration with grunt file

---
+++ dist/angularjs-scrum.js
@@ -0,0 +1,10 @@
+/*! angularjs-scrum - v1.0.0 - 2015-10-12
+ * https://github.com/prince1809/angularjs-scrum#readme
+ * Copyright (c) 2015 Prince Kumar (prince1809);
+ * Licensed 
+ */
+angular.module('templates.app', []);
+
+
+angular.module('templates.common', []);
+
